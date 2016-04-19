7dd9d2964ebb017c533c51792452c42a58d4d026
MyrraWey <muravyovdmitr@gmail.com>
2016-04-19 15:57:22 +0300

    add fragment

---
+++ app/src/main/java/com/zinitsolutions/test/testapplication/PostsListFragment.java
@@ -0,0 +1,21 @@
+package com.zinitsolutions.test.testapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dmitrij on 4/19/16.
+ */
+public class PostsListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_posts_list,container, false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/zinitsolutions/test/testapplication/SingleFragmentActivity.java
+++ app/src/main/java/com/zinitsolutions/test/testapplication/SingleFragmentActivity.java
@@ -1,13 +1,25 @@
 package com.zinitsolutions.test.testapplication;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SingleFragmentActivity extends AppCompatActivity {
+public class SingleFragmentActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new PostsListFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
